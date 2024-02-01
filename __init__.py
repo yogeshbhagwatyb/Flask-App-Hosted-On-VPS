5b754a81ec9f6ad11e9ba538656c062c94be7538
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2024-02-01 18:16:28 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ app.config['SECRET_KEY'] = "secretkey123"
 
 @app.route("/")  # this sets the route to this page
 def home():
-	return jsonify({'Message': "Testing"})
+	return jsonify({"Python application which is for testing purpose"})
 
 if __name__ == "__main__":
     app.run()
